021b4fe8d95c4b97693bb948f871e8a0be886a05
SrilekhaS20 <lekhaconst.1@gmail.com>
2025-06-21 20:08:05 +0530

    step-01: Created Flask app with health and version endpoints (v1.0.0)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# flask-docker-cicd
-Repo to demonstrate cicd pipeline
+# Flask Docker App - Manual to CI/CD
+A demo DevOps app to show how to deploy a Flask microservice to AWS EC2 using manual steps first, then automate using GitHub Actions.
+
+### Version
+v1.0.0 - Manual setup base version
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask App Deployed Manually to EC2"
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+@app.route("/version")
+def version():
+    try:
+        with open("version.txt") as file:
+            return file.read(), 200
+    except:
+        return "unknown", 200
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
